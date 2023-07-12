0a11e40fa1f6649f4f70056802ca96bbbaf6a86c
Nasya <nasyadina26@gmail.com>
2023-07-12 23:58:31 +0700

    uas

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\akuntansitransaksiController@index');
+    Route::get('akuntansitransaksi', 'App\Http\Controllers\akuntansitransaksiController@index');
+    
+    Route::get('akuntansitransaksi/tambah', 'App\Http\Controllers\akuntansitransaksiController@tambah');
+    Route::post('akuntansitransaksi/tambah_proses', 'App\Http\Controllers\akuntansitransaksiController@tambah_proses');
+    Route::get('akuntansitransaksi/edit/{id}', 'App\Http\Controllers\akuntansitransaksiController@edit');
+    Route::post('akuntansitransaksi/edit_proses', 'App\Http\Controllers\akuntansitransaksiController@edit_proses');
+    Route::get('akuntansitransaksi/delete/{id}', 'App\Http\Controllers\akuntansitransaksiController@delete');
+    
+
+
